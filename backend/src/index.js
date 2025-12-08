de868deda1427f0a24b4060363872359caf88cdb
Sayancode2026 <sayanbardhan2002@gmail.com>
2025-12-08 16:06:47 +0530

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -50,13 +50,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
 app.use((req, res) => {
   res.status(404).json({ 
     success: false,
